31e41c7dc4463dd36228c8f3dc385de2c481f651
Indrek Lasn <lasnindrek@gmail.com>
2019-07-07 16:27:10 +0300

    init

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 class WebComponentButton extends HTMLElement {
 
-  get enabled() {
-    console.log(val)
-    return this.hasAttribute('enabled')
-  }
-  set enabled(val) {
-    this.setAttribute('disabled');
-  }
-
-  get disabled() {
-    return this.hasAttribute('disabled');
-  }
-  set disabled(val) {
-    console.log(val)
-    this.setAttribute('disabled', '');
-  }
-  
   constructor() {
     super()
     this.addEventListener('click', () => {
-      if (this.disabled) {
-        return;
-      }
       console.log('hi')
     })
   }
@@ -37,11 +18,6 @@ class WebComponentButton extends HTMLElement {
 
   // When the element is either removed from the DOM, or moved to a different page:
   adoptedCallback() {} 
-
-  static get observedAttributes() {
-    return ['disabled', 'enabled'];
-  }
-
 
 }
 
